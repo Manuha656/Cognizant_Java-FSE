8fc6a8e186cb60bc38e0badb345d0c892969da05
Manuha656 <enjamurimanuha@gmail.com>
2026-06-08 22:24:30 +0530

    React till task4

--- Deepskilling/React/reactproject/src/App.js
+++ Deepskilling/React/reactproject/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+// import Home from './task2/Home';
+// import About from './task2/About';
+// import Contact from './task2/Contact';
+// import CalculateScore from './task3/CalculateScore';
+import Posts from './task4/Posts';
 
 function App() {
   return (
     <div className='container'>
-        <Home/>
+        {/* <Home/>
         <About/>
         <Contact/>
+        <CalculateScore name={"Manu"} school={"CVR"} total={300} goal={3} /> */}
+        <Posts/>
     </div>
   );
 }
