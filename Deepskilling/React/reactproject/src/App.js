5297b0aea9237b4812e89dd490057702fb3d79c3
Manuha656 <enjamurimanuha@gmail.com>
2026-06-09 22:27:47 +0530

    Remove class files and node_modules

--- Deepskilling/React/reactproject/src/App.js
+++ Deepskilling/React/reactproject/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
 // import Home from './task2/Home';
 // import About from './task2/About';
 // import Contact from './task2/Contact';
 // import CalculateScore from './task3/CalculateScore';
-import Posts from './task4/Posts';
+// import Posts from './task4/Posts';
+import CohortDetails from './task5/CohortDetails';
 
 function App() {
   return (
@@ -11,8 +11,16 @@ function App() {
         {/* <Home/>
         <About/>
         <Contact/>
-        <CalculateScore name={"Manu"} school={"CVR"} total={300} goal={3} /> */}
-        <Posts/>
+        <CalculateScore name={"Manu"} school={"CVR"} total={300} goal={3} />
+        <Posts/> */}
+        <CohortDetails cohort={{
+          currentStatus: "Ongoing",
+          cohortCode: 1021,
+          technology: "tech",
+          startDate: "01-10-2026",
+          coachName: "Manu",
+          trainerName: "Akki"
+        }}/>
     </div>
   );
 }
